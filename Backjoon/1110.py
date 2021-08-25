4aa27febc1f9487760100c6517198b4f5874dabd
taesunpark <tovbskvb@daum.net>
2021-08-26 08:00:01 +0900

    [update] int를 활용한 풀이 추가

--- Backjoon/1110.py
+++ Backjoon/1110.py
@@ -1,27 +1,14 @@
-number = input()
-number2 = int(number)
+number = int(input())
+value = number
 count = int(0)
-sum1 = 0
-
-if len(number) == 1:
-    number = number + str(0)
 
 while True:
-
-    first = number[0]
-    second = number[1]
-
-    sum1 = (int(second) * 10) + (int(first) + int(second)) % 10
-
+    first = number % 10
+    second = number // 10
+    third = first + second
+    number = first * 10 + third % 10
     count = count + 1
-    number = str(sum1)
 
-    if len(number) == 1:
-        number = str(0) + number
-
-    if sum1 == int(number2) and count > 1 and len(str(number2)) == 1:
-        print(count-1)
-        break
-    elif sum1 == int(number2) and count > 1:
+    if value == number:
         print(count)
         break
