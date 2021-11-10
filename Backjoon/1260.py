b93a8d2cb6f2921fdf178d3fc64b305925db4c83
taesunpark <tovbskvb@daum.net>
2021-11-10 19:11:32 +0900

    [add] 1260

---
+++ Backjoon/1260.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+
+def dfs(start_v, visited=[]):
+    visited.append(start_v)
+    print(start_v, end=" ")
+    for w in range(len(graph[start_v])):
+        if graph[start_v][w] == 1 and (w not in visited):
+            dfs(w, visited)
+
+
+def bfs(start_v):
+    visited = [start_v]
+	queue = deque()
+    queue.append(start_v)
+
+    while queue:
+        v = queue.popleft()
+        print(v, end=" ")
+        for w in range(len(graph[v])):
+            if graph[v][w] == 1 and (w not in visited):
+                visited.append(w)
+                queue.append(w)
+
+
+N, M, V = map(int, input().split())
+
+graph = [[0] * (N + 1) for _ in range(N + 1)]
+
+for _ in range(M):
+    m1, m2 = map(int, input().split())
+    graph[m1][m2] = 1
+    graph[m2][m1] = 1
+
+dfs(V)
+print()
+bfs(V)
