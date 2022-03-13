e189e1576db728218c129bea5f25ec1541e4d331
taesunpark <tovbskvb@daum.net>
2022-03-13 20:58:54 +0900

    [add] 11568

--- codingtest/src/BJ15664.java
+++ codingtest/src/BJ15664.java
@@ -2,26 +2,58 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.HashSet;
 
 public class BJ15664 {
 
-    public static StringTokenizer st;
+    static int n,m;
+    static boolean[] isUsed;
+    static int[] array;
+    static int[] result;
+    static HashSet hashSet;
 
     public static void main(String[] args) throws IOException {
-        int N, M, index=0;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        int[] array = new int[N];
+        String[] a = br.readLine().split(" ");
+        n = Integer.parseInt(a[0]);
+        m = Integer.parseInt(a[1]);
+        isUsed = new boolean[10];
+        array = new int[n];
+        result = new int[10];
+        hashSet = new HashSet();
+        String[] b = br.readLine().split(" ");
 
-        st = new StringTokenizer(br.readLine());
-
-        while (st.hasMoreTokens()){
-            array[index++] = Integer.parseInt(st.nextToken());
+        for (int i=0; i<b.length; i++){
+            array[i] = Integer.parseInt(b[i]);
         }
+
         Arrays.sort(array);
 
+        bfs(0);
+    }
+    public static void bfs(int k){
+        if (k == m){
+            if (!hashSet.contains(result)){
+                hashSet.add(result);
+                for (int i=0; i<m; i++){
+                    System.out.print(result[i]+" ");
+                }
+                System.out.print("\n");
+                return;
+            }
+            return;
+        }
+
+        for (int i=0; i<n; i++){
+            if (!isUsed[i]){
+                isUsed[i] = true;
+                result[i] = array[i];
+
+                System.out.println(result[i]+"hi");
+
+                bfs(k+1);
+                isUsed[i] = false;
+            }
+        }
     }
 }
