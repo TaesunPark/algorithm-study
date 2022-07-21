e0d386bbfd3fa147a34e540cd98fe150898f19d2
tovbskvb@daum.net <tovbskvb@daum.net>
2022-07-21 23:29:54 +0900

    [ADD] BJ3986

--- codingtest/src/바킹독/연습문제0x08/BJ3986.java
+++ codingtest/src/바킹독/연습문제0x08/BJ3986.java
@@ -1,2 +1,39 @@
-package 바킹독.연습문제0x08;public class BJ3986 {
+package 바킹독.연습문제0x08;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class BJ3986 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Character> stack = new Stack<>();
+        int loopCnt = Integer.parseInt(br.readLine());
+
+        int cnt =0;
+
+        for (int i=0; i<loopCnt; i++){
+            String str = br.readLine();
+
+            for (int j=0; j<str.length();j++){
+                boolean check = false;
+                while (!stack.isEmpty() && stack.peek() == str.charAt(j)){
+                    stack.pop();
+                    check = true;
+                }
+                if (check == true) continue;
+
+                stack.push(str.charAt(j));
+
+            }
+            if (stack.isEmpty()){
+                cnt++;
+            }else{
+                stack.clear();
+            }
+        }
+        System.out.print(cnt);
+    }
 }
