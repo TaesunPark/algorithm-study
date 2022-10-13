72cfb4bdd162ad2afc5cc81ae4fe7a6107178466
tovbskvb@daum.net <tovbskvb@daum.net>
2022-10-14 00:17:31 +0900

    [ADD]1620, 7785

--- codingtest/src/바킹독/해시/BJ1620.java
+++ codingtest/src/바킹독/해시/BJ1620.java
@@ -1,2 +1,26 @@
-package 바킹독.해시;public class BJ1620 {
+package 바킹독.해시;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class BJ1620 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        HashMap<String, String> hashMap = new HashMap<>();
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            String name = sc.next();
+            hashMap.put(Integer.toString(i), name);
+            hashMap.put(name, Integer.toString(i));
+        }
+
+        for(int i = 0; i < m; i++){
+            String a = sc.next();
+            System.out.println(hashMap.get(a));
+        }
+
+
+    }
 }
--- codingtest/src/바킹독/해시/BJ7785.java
+++ codingtest/src/바킹독/해시/BJ7785.java
@@ -1,2 +1,42 @@
-package 바킹독.해시;public class BJ7785 {
+package 바킹독.해시;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Scanner;
+
+public class BJ7785 {
+    public static void main(String[] args){
+        HashMap<String, Boolean> enterMap = new HashMap<>();
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++){
+            String value = sc.next() + " " + sc.next();
+
+            String[] values = value.split(" ");
+            if(values[1].equals("enter")){
+                enterMap.put(values[0], true);
+            } else{
+                enterMap.put(values[0], false);
+            }
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for(Entry<String, Boolean> entry : enterMap.entrySet()){
+            if(entry.getValue() == true){
+                list.add(entry.getKey());
+            }
+        }
+
+        Collections.sort(list, Collections.reverseOrder());
+
+        for(int i = 0; i < list.size(); i++){
+            System.out.println(list.get(i));
+        }
+
+    }
 }
