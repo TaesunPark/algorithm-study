bda8f53b487c9d424c38fab75a7f0466dc6a670d
tovbskvb@daum.net <tovbskvb@daum.net>
2023-05-16 11:30:21 +0900

    [ADD] dp 크리보드

---
+++ codingtest/src/BJ11058.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ11058 {
+    public static void main(String[] args) throws IOException {
+        int N;
+        long result = 0;
+        long[] dp = new long[101];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        dp[4] = 4;
+        dp[5] = 5;
+
+        for (int i = 6; i <= 100; i++){
+            dp[i] = Math.max(dp[i-3] * 2, dp[i-4] * 3);
+            dp[i] = Math.max(dp[i-5] * 4, dp[i]);
+        }
+
+        System.out.print(dp[N]);
+
+
+    }
+}
